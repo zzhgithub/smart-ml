69ef811755ea387f198baf16f63df92789c52260
周子豪 <1042181618@qq.com>
2017-08-04 17:05:22 +0800

    finish public method read-data

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # SMART_ML
-使用common Lisp 写的机器学习库。使用全部的中文注释。
+
+  使用common Lisp 写的机器学习库。使用全部的中文注释。
+
+# Public(公共方法)
+
+## read-data(阅读数据)
+
+  介绍测试读取数据源的格式是否正确，不产生学习结果和副作用。可以用来输出数据的基本信息。包括数据个数和特征维度。(暂时不对复杂的数据结构进行支持)
+  
+  示例：
+  ```common-lisp
+  ;; 非监督学习 时的一个数据源
+  (smart-ml:read-data #P"test.csv")
+  ;; 监督学习 时的两个源 输入和输出分开
+  (smart-ml:read-data #P"test.csv" #P"test2.csv")
+  ;; 其中要求两个数据的样本数是一致的不然会报错
+  ;; check-bad
+  ```
+
+# Perceptron(感知器)
+
+## todo
--- public.lisp
+++ public.lisp
@@ -1,19 +1,18 @@
 (in-package :smart-ml)
 
 ;; 公共方法
-(ql:quickload "cl-csv")
-
 
 (defun dump-data-info (x)
-  "打印出入参数据的信息，方便计算和统计。"
-  ;;todo
-  )
+  "打印出入参数据的信息，方便计算和统计。元素个数，维度特征数"
+  (let ((feature 0)
+        (n (list-length x)))
+    ;; 输出结果
+    (dolist (tmp x 'done)
+      (setf feature (list-length tmp)))
+    (format t "特征空间大小为~S~%样本空间大小为~S~%" feature n)))
 
-
-(defun check-data (x y)
-  "计算两个数据的长度是不是一致的。和检查每一个数据的特征维数是否一致。"
-  ;;todo
-  )
+(defmacro check-len (x y)
+  `(not (eql (list-length ,x) (list-length ,y))))
 
 (defun read-data (&key x (y nil y-p))
   "读取文件数据的方法，用于生成文件的原始数据的信息。必填字段X表示学习过程中的样本的特征空间数据，Y可选字段表示输出空间数据"
@@ -21,12 +20,11 @@
       ;如果传来了两个值要对数据进行验证
       (let ((tmp-x (cl-csv:read-csv x))
             (tmp-y (cl-csv:read-csv y)))
-        (if (check-data tmp-x tmp-y)
+        (if (check-len tmp-x tmp-y)
             'check-bad
             (progn
               (dump-data-info tmp-x)
-              (dump-data-info tmp-x)
-              nil)))
+              (dump-data-info tmp-x))))
       (let ((tmp-x (cl-csv:read-csv x)))
         (dump-data-info tmp-x))))
 
